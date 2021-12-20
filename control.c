40b612006d26039abf84a135f33a5973e5b51236
kszeto20 <kszeto20@stuy.edu>
2021-12-19 21:26:02 -0500

    create works?

--- control.c
+++ control.c
@@ -1,16 +1,41 @@
 #include "control.h"
 
 void create() {
-  int semID = semget(SEM_KEY, 1, IPC_CREAT | IPC_EXCL | 0644);
-  if (semID == -1) {
-    semID = semget(SEM_KEY, 1, 0);
+
+  // file open
+  int fd = open("story.txt", O_CREAT | O_TRUNC, 0644);
+  if (errno != 0) {
+      printf("Error: %s\n", strerror(errno));
+      exit(errno);
+  }
+  // semaphore create
+  int semd = semget(SEM_KEY, 1, IPC_CREAT | IPC_EXCL | 0644); // will fail if already exits
+    // if exists; do this
+  if (semd == -1) {
+    semd = semget(SEM_KEY, 1, 0);
   }
 
   union semun us;
-  us.val = 10;
-  int r = semctl(semID, 0, GETVAL, us);
+  us.val = 1;
+  int r = semctl(semd, 0, SETVAL, us);
+  printf("got here\n");
 
-  printf("SEMCTL RETURNED: %d\n", r);
+  //shared memory create
+  int shmd = shmget(SHM_KEY, 1, IPC_CREAT | IPC_EXCL | 0644);
+  // if exists, do this
+  if (shmd == -1) {
+    shmd = shmget(SHM_KEY, 0, 0);
+  }
+
+  printf("got here1\n");
+  // link to descriptor
+  int *data = shmat(shmd, 0, 0);
+
+  printf("got here2\n");
+  close(fd);
+  // assign to beginning
+  (*data) = 0;
+  printf("got here3\n");
 }
 
 int main() {
