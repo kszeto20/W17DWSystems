c50345812cfc5a52747da36142f6a0cf0e32ce5b
kszeto20 <kszeto20@stuy.edu>
2022-01-01 09:53:30 -0500

    create working

--- control.c
+++ control.c
@@ -3,39 +3,48 @@
 void create() {
 
   // file open
-  int fd = open("story.txt", O_CREAT | O_TRUNC, 0644);
+  int fd = open("story.txt", O_CREAT | O_EXCL | O_TRUNC, 0644);
   if (errno != 0) {
-      printf("Error: %s\n", strerror(errno));
-      exit(errno);
+      fd = open("story.txt", O_RDWR);
+      printf("Opened File\n");
+  } else {
+    printf("Opened File\n");
   }
+
   // semaphore create
   int semd = semget(SEM_KEY, 1, IPC_CREAT | IPC_EXCL | 0644); // will fail if already exits
     // if exists; do this
-  if (semd == -1) {
+  if (semd < 0) {
     semd = semget(SEM_KEY, 1, 0);
   }
+  if (semd == 0) {
+    printf("Created Semaphore\n");
+  }
 
+  // semaphore set
   union semun us;
   us.val = 1;
   int r = semctl(semd, 0, SETVAL, us);
-  printf("got here\n");
+  printf("Set Semaphore\n");
+  //printf("got here\n");
 
   //shared memory create
   int shmd = shmget(SHM_KEY, 1, IPC_CREAT | IPC_EXCL | 0644);
   // if exists, do this
-  if (shmd == -1) {
+  if (shmd < 0) {
     shmd = shmget(SHM_KEY, 0, 0);
   }
 
-  printf("got here1\n");
   // link to descriptor
   int *data = shmat(shmd, 0, 0);
-
-  printf("got here2\n");
-  close(fd);
+  //printf("got here1\n");
+  //printf("got here2\n");
   // assign to beginning
   (*data) = 0;
-  printf("got here3\n");
+  printf("created shared memory\n");
+  //printf("got here3\n");
+
+  close(fd);
 }
 
 int main() {
